7343e72da1ab54a9cb2f9fabd20c390c099b6e88
Benj <Benjamin.Schwerdtner@gmail.com>
2019-04-21 15:49:26 +0200

    added nicholas supperior gtags func

--- layers/my-tags-layer/funcs.el
+++ layers/my-tags-layer/funcs.el
@@ -1,7 +1,6 @@
 (defvar cos-gtags-config-file "~/.tracked/.cos-gtags-config")
 (defvar my-gtags-command "gtags --gtagslabel pygments --gtagsconf %s")
 
-
 (defun my-regenerate-idlegame-tags-async ()
   "Regenerate cos gtags, assumes cos-gtags-config is set"
   (interactive)
@@ -13,6 +12,31 @@
 ;;ggtags-highlight-tag nil ;;maybe
 
 (setq-default ggtags-auto-jump-to-match 'first)
+
+
+;;nicholas-stuff
+(defvar idle-game-best-folders '( "Assets/#/Sources" "Assets/#/Scripts" "Assets/Editor" ))
+
+(defun idle-game-folders ()
+  (mapconcat (lambda (path) (format "\"%s\"" (concat idlegame-project-root path))) idle-game-best-folders " "))
+
+
+
+
+(defun regenerate-idlegame-gtags (&optional dirs)
+  "First output the files in DIRS we want to parse to gtags.files, then run gtags in idle-game-project-root."
+  (interactive)
+  (let* ((directories (or dirs (idle-game-folders)))
+         (default-directory (projectile-project-root))
+         (find-command (format "fd .cs %s > %sgtags.files" directories default-directory))
+         (gtags-command (format "gtags -v --gtagslabel pygments %s" (directory-file-name default-directory))))
+    (message (format "find files command %s" find-command))
+    (shell-command find-command)
+    (shell-command gtags-command)
+    ))
+
+
+
 
 
 (defun compilation-maybe-halt-auto-jump (buffer pos)
