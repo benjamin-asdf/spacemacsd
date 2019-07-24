afc46468b7305bc66e562fc5d8856b1f46c7b335
Benj <Benjamin.Schwerdtner@gmail.com>
2019-07-24 18:09:34 +0200

    Rename layer to redshiftel

--- init.el
+++ init.el
@@ -75,7 +75,7 @@ This function should only modify configuration layer settings."
      my-tags-layer
      laptop-configs
      my-external-tools
-     redshifter)
+     redshiftel)
 
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
