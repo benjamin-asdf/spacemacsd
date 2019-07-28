6cb7819e2e26c2eb2ea5920aad1ff083504811ca
Benj <Benjamin.Schwerdtner@gmail.com>
2019-07-28 19:40:05 +0200

    Add code for basic creating banners

--- layers/best-banners/funcs.el
+++ layers/best-banners/funcs.el
@@ -1,3 +1,5 @@
+(defconst best-messages-file "~/.config/my-messages")
+(defconst banner-header "\n             Welcome to spacemacs.\n")
 
 
 
@@ -18,30 +20,13 @@ If the file specified in best-banners-messages-file matches the saved file, we t
 
 
 
-
-
-
-(defconst best-messages-file "~/.config/my-messages")
-
-
-(defconst banner-header "\n\nWelcome to spacemacs")
-
 (defun new-banner-file-name ()
-  (format "./banners%s" "000"))
+  (format "./banners/%s" "000"))
 
-(defun create-banner ()
-  (let (banner
-        (concat banner-header (shell-command-to-string "figlet msg"))
-        banner-file (new-banner-file-name))
-    (with-temp-file banner-file
-      (insert-file-contents banner-file msg))))
-
-(create-banner)
-
-(defun my-best-message ()
-  "Get a display message"
-  (interactive)
-  (substring (shell-command-to-string "fish -c best-message") 0 -1))
+(defun banners-create-new (msg)
+  (let ((banner (concat banner-header (shell-command-to-string (format "figlet \"%s\"" msg))))
+        (banner-file (new-banner-file-name)))
+    (write-region banner nil banner-file)))
 
 (defun best-message()
   (let ((msgs (read-lines best-messages-file)))
