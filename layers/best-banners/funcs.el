732e7a8fbb043e327dd7aab4251e1f205ed80fac
Benj <Benjamin.Schwerdtner@gmail.com>
2019-08-01 11:34:44 +0200

    Checkout banners code

--- layers/best-banners/funcs.el
+++ layers/best-banners/funcs.el
@@ -1,31 +1,39 @@
-(defconst best-messages-file "~/.config/my-messages")
+(defconst best-messages-file "/home/benj/.homesick/repos/dotfiles/home/.config/my-messages")
 (defconst banner-header "\n             Welcome to spacemacs.\n")
-
-
+(defconst banners-dir "~/.spacemacs.d/layers/best-banners/banners")
 
 
 (defun best-banners--save-file-hook ()
   "This hook is supposed to run when savin a file.
 If the file specified in best-banners-messages-file matches the saved file, we try to create banners"
-  (message "save file hook..."))
+  (when (equal buffer-file-name best-messages-file)
+    (my-delete-all-files banners-dir)
+    (dolist (msg (read-lines best-messages-file))
+      (banners-create-new msg))))
 
 (add-hook 'after-save-hook 'best-banners--save-file-hook)
 
 
 
+(defun my-delete-all-files (dir)
+  (dolist (elem (directory-files dir))
+    (unless (member elem '("." ".."))
+      (delete-file (concat (file-name-as-directory dir) elem)))))
 
+(defun new-banner-file-name (&optional n)
+  ;; FIXME
+  (let ((n (or n 0))
+        (message (type-of n))
+        (file (format "%s/%d" banners-dir n)))
+    (while (file-exists-p file)
+      (setq file (new-banner-file-name (+ 1 n))))
+    file))
 
-
-
-
-
-
-(defun new-banner-file-name ()
-  (format "./banners/%s" "000"))
 
 (defun banners-create-new (msg)
   (let ((banner (concat banner-header (shell-command-to-string (format "figlet \"%s\"" msg))))
-        (banner-file (new-banner-file-name)))
+        ;; FIXME
+        (banner-file (new-banner-file-name 0)))
     (write-region banner nil banner-file)))
 
 (defun best-message()
@@ -34,3 +42,9 @@ If the file specified in best-banners-messages-file matches the saved file, we t
 
 (defun rand-element (list)
   (nth (random (length list)) list))
+
+
+;; TODO s
+;; random figlet fonts
+;; prompt before creating files
+;; open a buffer with the progress output
