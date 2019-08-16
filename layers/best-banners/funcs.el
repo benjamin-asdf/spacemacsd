0f520dbe2077b56432ec53bae6c4a0924a19ce09
Benj <Benjamin.Schwerdtner@gmail.com>
2019-08-16 16:29:59 +0200

    Update best-banners

--- layers/best-banners/funcs.el
+++ layers/best-banners/funcs.el
@@ -1,18 +1,25 @@
-(defconst best-messages-file "/home/benj/.homesick/repos/dotfiles/home/.config/my-messages")
-(defconst banner-header "\n             Welcome to spacemacs.\n")
-(defconst banners-dir "~/.spacemacs.d/layers/best-banners/banners")
+;; (defconst best-messages-file "/home/benj/.homesick/repos/dotfiles/home/.config/my-messages")
 
+(defconst best-messages-file "/tmp/test-messages")
+(defconst banner-header "\n             Welcome to spacemacs.\n")
 
 (defun best-banners--save-file-hook ()
-  "This hook is supposed to run when saving a file.
+  "Check if the current file is message file, maybe create banners.
 If the file specified in best-banners-messages-file matches the saved file, we try to create banners"
-  (when (equal buffer-file-name best-messages-file)
-    (my-delete-all-files banners-dir)
-    (dolist (msg (read-lines best-messages-file))
-      (banners-create-new msg))))
+  (when (and (equal buffer-file-name best-messages-file)
+             (yes-or-no-p "Saved best-messages file, do you want to recreate spacemacs startup banners?"))
+    (best-banners-recreate)))
 
 (add-hook 'after-save-hook 'best-banners--save-file-hook)
 
+(defun best-banners-recreate ()
+  "Recreate banners from best-message file."
+  (interactive)
+  (unless (file-exists-p spacemacs-private-banner-directory)
+    (make-directory spacemacs-private-banner-directory))
+  (my-delete-all-files spacemacs-private-banner-directory)
+  (dolist (msg (read-lines best-messages-file))
+    (banners-create-new msg)))
 
 
 (defun my-delete-all-files (dir)
@@ -22,7 +29,7 @@ If the file specified in best-banners-messages-file matches the saved file, we t
 
 (defun new-banner-file-name (&optional n)
   (let* ((num (if n n 0))
-         (file (format "%s/%d" banners-dir num)))
+         (file (format "%s/%d.txt" spacemacs-private-banner-directory num)))
     (while (file-exists-p file)
       (setq file (new-banner-file-name (+ 1 num))))
     file))
