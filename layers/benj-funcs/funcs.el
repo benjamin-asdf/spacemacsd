cb9db4574ab51296708e8e68891d04e3d18171c5
Benj <Benjamin.Schwerdtner@gmail.com>
2019-08-19 22:55:29 +0200

    Improve copy word from above

--- layers/benj-funcs/funcs.el
+++ layers/benj-funcs/funcs.el
@@ -9,18 +9,27 @@
     (goto-char point)))
 
 (defun benj-copy-word-from-above ()
-  "Copy the word from line above."
+  "Copy the word or space from next non-empty line above."
   (interactive)
-  (let ((col (current-column)))
-    (save-excursion
-      (forward-line -1)
-      (evil-goto-column col)
-      ;; Take over spaces from above, but no newlines.
-      (kill-new
-       (if (string-equal (thing-at-point 'char) " ")
-           " "
-         (thing-at-point 'evil-word)))))
-  (yank))
+  (let ((word)
+        (col (current-column))
+        (line (1- (line-number-at-pos))))
+    (while (and (not (= line 0))
+            (not (setq word (benj--word-in-column line col))))
+      (setq line (1- line)))
+    (when word
+      (kill-new word)
+      (yank))))
+
+(defun benj--word-in-column (line col)
+  "Evaluates to the evil word in LINE and COL.
+ single space if char at point is a space. Nil for empty lines."
+  (save-excursion
+    (goto-line line)
+    (evil-goto-column col)
+    (cond ((looking-at "^$") nil)
+          ((string-equal (thing-at-point 'char) " ") " ")
+          (t (thing-at-point 'evil-word)))))
 
 
 (defun benj-read-lines (filePath)
