55ac4525664f2f179401f3653ab929dbc82bfe92
Benj <Benjamin.Schwerdtner@gmail.com>
2019-08-23 15:53:25 +0200

    Add first simple unity refresh thing

--- layers/benj-unity/funcs.el
+++ layers/benj-unity/funcs.el
@@ -1,52 +1,32 @@
 
-(defvar bunel-handle-path nil)
-
-(defun bunel-handle-path ()
-  (unless bunel-handle-path
-      (dolist (line (read-lines "~/repos/bunel/.config")) handle-path
-              (if (string-match "^HANDLE_FILE_PATH=\\(.+\\)$" line)
-                  (setq handle-path (match-string 1 line)))))
-  bunel-handle-path)
+(defconst bunel-handle-path "/tmp/bunel-handles")
 
 
+(defconst bunel-idlegame-projects '("IdleGame" "IdleGameSymbolicLink" "IdleGameSymbolicLink-Extra"))
 
 (defconst bunel-default-unity-project "IdleGame")
 
-(defun bunel--handle-create (cmd args project)
-  (let*
-      ((project (or project bunel-default-unity-project))
+;; (defun bunel-refresh-project (&optional project all)
+;;   "Refresh idlegame PROJECT. If ALL is non nil, refresh all projects."
+;;   (mapcar (lambda (project)
+;;             ()))
+;;   )
 
-       (handle (cons "command" cmd)
-               ())
-       (handle '(()))
-       (handle '(("command" )))
-       (handle
+(defun bunel-create-handle-file (project)
+  "Create a handle file for project."
+  (unless (file-exists-p bunel-handle-path) (make-directory bunel-handle-path))
+  (write-region "" nil (concat (file-name-as-directory bunel-handle-path) project)))
 
-        '(("command" . InfuseCrystall) (args meme-plasma restructure-patterns))))
-    (write-region
-     (json-encode-alist handle) nil )))
-
-
-(defun params-test (first-param args &rest the-rest)
-  (mapcar (lambda (arg) (message arg)) args))
+(defun bunel-refresh-client ()
+  "Refresh default idlegame unity project."
+  (interactive)
+  (bunel-create-handle-file bunel-default-unity-project))
 
 
-(params-test "hello" '("world" "motherfuckers") 'some-rest 'another-rest)
 
-
-(write-region (json-encode-alist '(()) nil (bunel-handle-path)))
-
-(write-region (json-encode-alist '((lul . hi))) nil (concat (file-name-as-directory (bunel-handle-path)) "IdleGame"))
-
-(progn
-  (unless (file-exists-p (bunel-handle-path))
-    (make-directory (bunel-handle-path)))
-  (write-region "" nil (concat (file-name-as-directory (bunel-handle-path)) bunel-default-unity-project))
-  )
-
-
-;; (defun crystall-plant ()
-;;   (interactive)
-;;   (if t
-;;       (error "A seed is growing.")
-;;     (message "not implemented")))
+;; (defun bunel-handle-path ()
+;;   (unless bunel-handle-path
+;;     (dolist (line (benj-read-lines "~/repos/bunel/.config")) handle-path
+;;             (if (string-match "^HANDLE_FILE_PATH=\\(.+\\)$" line)
+;;                 (setq handle-path (match-string 1 line)))))
+;;   bunel-handle-path)
