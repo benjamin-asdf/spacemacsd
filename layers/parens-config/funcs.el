5fcb91fc3af5c588294cccfbeba9e3846c40d90b
Benj <Benjamin.Schwerdtner@gmail.com>
2020-04-23 08:03:12 +0200

    Add parens config, temp fix

---
+++ layers/parens-config/funcs.el
@@ -0,0 +1,6 @@
+;; FIXME make it work, crashes atm around line delimeters
+(show-smartparens-global-mode -1)
+
+;; sticking to show paren mode instead
+(set-face-foreground 'show-paren-match "White")
+(set-face-underline 'show-paren-match "White")
