67748fd964c817641686b25ae5fc00d057526132
Benj <Benjamin.Schwerdtner@gmail.com>
2020-04-25 20:10:46 +0200

    Add shitty func for writing the achv poggers systems

--- lisp/temp-stuff/achvpoggers.el
+++ lisp/temp-stuff/achvpoggers.el
@@ -1,33 +1,9 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 (defconst move-achv-code-getter-template
   "
-            c.TryGetAchvProgressView<%s>(out var view) {
+        _updateView() {
+            if (!c.TryGetAchvProgressView(%s, out var view)) return;
 %s
-            }
+        }
 "
   )
 
@@ -36,90 +12,34 @@
   "Reformat update view code to fit new api."
   (interactive)
 
-  (re-search-forward "AddReactEach(Matcher.AllOf<AchievementProgress>(),")
-  (let ((text) (min) (max) (achvname))
-    (save-excursion
-      (re-search-forward "AddReactEach.*UpdateView>(),")
-      (evil-end-of-line)
+  (let ((text) (min) (max) (func-spot) (achvname))
+    (while (re-search-forward "AddReactEach(Matcher\.AllOf<\\(\\w+\\).*UpdateView>(), e => {" nil t)
+      (setq achvname (match-string 1))
       (save-excursion
-        ;; ommit the "var view =" line
-        (forward-line 2)
-        (setq min (point-at-bol)))
-      (evil-jump-item)
-      (save-excursion
-        (forward-line -1)
-        (setq max (point-at-eol))))
-    (setq text
-          (replace-regexp-in-string
-           "\\(^ \\)" "\t"
-           (buffer-substring min max)))
-    (re-search-forward "c.state.UpdateView<\\(\\w+C\\)>();$")
-    (replace-match (format move-achv-code-getter-template (match-string 1) text))
+        (setq func-spot (copy-marker (point-at-eol)))
+        (evil-end-of-line)
+        (save-excursion
+          ;; ommit the "var view =" line
+          (forward-line 2)
+          (setq min (copy-marker (point-at-bol))))
+        (evil-jump-item)
+        (save-excursion
+          (forward-line -1)
+          (setq max (copy-marker (point-at-eol)))))
+      (setq text (buffer-substring min max))
+      (kill-region (save-excursion
+                     (goto-char min)
+                     (forward-line -3)
+                     (point-at-bol))
+                   (save-excursion
+                     (goto-char max)
+                     (forward-line 1)
+                     (point-at-eol)))
 
-    (re-search-forward "AddReactEach.*UpdateView>(),")
-    (kill-region (point-at-bol) (save-excursion ) ))
+      (goto-char func-spot)
+      (insert (format move-achv-code-getter-template achvname text))))
+
+  (goto-char (point-min))
+  (while (re-search-forward "c.state.UpdateView<\\w+C>();" nil t)
+    (replace-match "_updateView();"))
   )
-
-
-
-
-
-;; (progn (re-search-forward "c.state.UpdateView<\\(\\w+\\)C>();$")
-;;        (replace-match (format best-template (match-string 1)))
-;;        )
-
-;; (defconst best-template "hello
-;; %s")
-
-;; c.state.UpdateView<ChurchAchievementProgressBarC>();
-
-;; (save-excursion
-;;   (forward-line 10)
-;;   (point)
-;;   )
-
-
-;; (progn (forward-line 3)
-;;        (insert best-str))
-
-
-;; (count-lines )
-
-
-
-;; (setq best-str " best
-;;   next line
-;; ")
-
-;; best-text
-
-
-;; (concat ?\n (make-string 4 ?\t) "}")
-
-;; (replace-regexp-in-string "\\(^\\s+\\)" (make-string 4 ?\t ) best-text)
-
-
-;; (progn (forward-line 2) (insert "something" ?\n "more"))
-
-
-
-
-
-;; ;; (progn (forward-line) (insert best-text))
-;; ;; 				Contexts achC = c.GetAchievementContext(view.GetIdentifier());
-;; ;; 				view.UpdateView(achC.activeChurchSlotSpinsAchievement);
-
-
-
-;; ;;         (re-search-forward "c.state.UpdateView<\\(\\w+C\\)>();$")
-;; ;;         (replace-match (format move-achv-code-getter-template (match-string 1) text)))
-
-
-
-
-
-;; (progn (re-search-forward "best") (kill-region (point-at-bol) (point-at-eol))
-;;        (insert "hello mofos"))
-
-
-;; "best"
