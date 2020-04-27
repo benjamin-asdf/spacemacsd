08aeb983d8bc9f2f00df4d1d2fb4fab7c022a92b
Benj <Benjamin.Schwerdtner@gmail.com>
2020-04-27 06:50:19 +0200

    Add func to fix acvh comps

--- lisp/temp-stuff/achvpoggers.el
+++ lisp/temp-stuff/achvpoggers.el
@@ -43,3 +43,34 @@
   (while (re-search-forward "c.state.UpdateView<\\w+C>();" nil t)
     (replace-match "_updateView();"))
   )
+
+
+(defconst dirs-with-monos '("/home/benj/idlegame/IdleGame/Assets/#/Sources/Achievements/MonoBehaviours/AchievementProgressDisplayImpl/Icons/" "/home/benj/idlegame/IdleGame/Assets/#/Sources/Achievements/MonoBehaviours/AchievementProgressDisplayImpl/ProgressBars/"))
+
+
+
+(defun benj-achv-replace-obsolete-monos ()
+  "Replace syntax for acvh icons and bars, put obsolete attr."
+  (interactive)
+  (dolist (dir dirs-with-monos)
+    (dolist (file (benj-directory-files dir))
+      (unless (string-equal "meta" (file-name-extension file))
+        (with-temp-file file
+          (insert-file-contents-literally file)
+          (unless (search-forward "[Obsolete]" nil t)
+            (goto-char (point-min))
+            (set-buffer-file-coding-system 'utf-8)
+            (while (re-search-forward "\r\n" nil t) (replace-match "\n"))
+            (if (not (search-forward "Component" nil t))
+                (message "Did not find  Component in %s" file)
+              (progn (forward-line -2)
+                     (insert "using System;\n")
+                     (forward-line 1)
+                     (insert "\n[Obsolete]")
+                     (cond ((re-search-forward "AchievementProgressBar<\\w+>")
+                            (replace-match "AchvProgressBar"))
+                           ((re-search-forward "AchievementProgressIcon<\\w+>")
+                            (replace-match "AchvProgressIcon")))
+                     (unless (search-forward "// special")
+                       (goto-line 6)
+                       (insert "[Obsolete]\n"))))))))))
