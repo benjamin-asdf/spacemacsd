d7d309622eb3df30e7b2ddc7bdda25d5ed05b4ae
Benj <Benjamin.Schwerdtner@gmail.com>
2020-05-06 20:24:29 +0200

    Add omnisharp bindings

--- layers/csharp-config/keybindings.el
+++ layers/csharp-config/keybindings.el
@@ -3,5 +3,7 @@
 (spacemacs/set-leader-keys-for-major-mode 'csharp-mode "or" 'benj-dotnet-run)
 (spacemacs/set-leader-keys-for-major-mode 'csharp-mode "oe" 'benj-chsarp-exclude-region)
 (spacemacs/set-leader-keys-for-major-mode 'csharp-mode "oE" 'benj-csharp-exclude-buffer)
+(spacemacs/set-leader-keys-for-major-mode 'csharp-mode "oo" 'omnisharp-show-overloads-at-point)
+(spacemacs/set-leader-keys-for-major-mode 'csharp-mode "ol" 'omnisharp-show-last-auto-complete-result)
 
 (spacemacs/set-leader-keys "onc" 'benj-create-new-chsarp-sample)
