c13f62fbf50216e3091ef4874e08cfa0a821a080
Benj <Benjamin.Schwerdtner@gmail.com>
2020-05-08 10:21:58 +0200

    Add benj-trello layer

---
+++ layers/benj-trello/funcs.el
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+
+
+
+
+
+(defconst benj-org-trello-directory (concat (file-name-as-directory org-directory) "trello"))
+
+(defconst org-)
+
+
+(defconst best-ben-org-trello-file "/home/benj/org/trello/ben.org")
+
+(defun benj-async-pull-trello ()
+  (interactive)
+  (copy-file )
+  (async-start
+   (lambda ()
+     (with-temp-file
+         (org-trello-sync-buffer))))
+  'ignore)
+
+
+
+
+(run-at-time )
