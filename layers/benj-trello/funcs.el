149bc068296704bad0bb998d1da3678a6823eb50
Benj <Benjamin.Schwerdtner@gmail.com>
2020-05-09 13:29:43 +0200

    Fix typos and stuff

--- layers/benj-trello/funcs.el
+++ layers/benj-trello/funcs.el
@@ -10,23 +10,27 @@
 
 
 
-(defconst benj-org-trello-directory (concat (file-name-as-directory org-directory) "trello"))
+(defconst benj-trello-directory (concat (file-name-as-directory org-directory) "trello"))
 
-(defconst org-)
+(defconst benj-trello-ben-board-file (concat (file-name-as-directory benj-trello-directory) "ben.org"))
 
-
-(defconst best-ben-org-trello-file "/home/benj/org/trello/ben.org")
 
 (defun benj-async-pull-trello ()
+  "Pull ben trello boad async.
+`benj-trello-ben-board-file' needs to be an initialzied org-trello board file"
   (interactive)
-  (copy-file )
+  (copy-file benj-trello-ben-board-file (concat benj-trello-ben-board-file ".old") t)
   (async-start
    (lambda ()
-     (with-temp-file
+     (with-temp-file benj-trello-ben-board-file
+       (insert-file-contents-literally benj-trello-ben-board-file)
          (org-trello-sync-buffer))))
-  'ignore)
+  (lambda (res)
+    (message "Finished syncing trello file.")
+    )
+  )
 
 
 
 
-(run-at-time )
+;; (run-at-time )
