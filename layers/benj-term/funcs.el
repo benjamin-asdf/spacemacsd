1a9f54dfdf599f7550922836b167a7d6bf1bc12f
Benj <Benjamin.Schwerdtner@gmail.com>
2020-06-08 14:07:45 +0200

    Add commented out stuff, todo

---
+++ layers/benj-term/funcs.el
@@ -0,0 +1,13 @@
+
+;; TODO
+
+
+;;(with-eval-after-load 'evil-core
+ ;; (evil-set-initial-state 'term-mode 'emacs))
+
+;; (evil-set-initial-state 'term-mode 'emacs)
+
+;; (add-hook )
+
+;; not how it works
+;; (add-hook 'window-selection-change-functions #'(lambda (window) (message (buffer-name (window-buffer window)))))
