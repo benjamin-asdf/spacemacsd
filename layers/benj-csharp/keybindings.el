c03fbfb551b0febaa0e57fbf0e9cd5c2b2d85448
Benj <Benjamin.Schwerdtner@gmail.com>
2020-06-10 13:42:42 +0200

    Fix string format

--- layers/benj-csharp/keybindings.el
+++ layers/benj-csharp/keybindings.el
@@ -15,5 +15,5 @@
           (spacemacs/set-leader-keys (concat benj-csharp-leader-keys (car x)) (cdr x)))
         '(("p" . benj-chsarp-msbuild-this-proj)
           ("u" . benj-csharp/replace-wl-with-debug)
-          ("c" . (lambda (interactive) (benj//replace-match-buff "UnityEngine.Debug.Log" "Console\\.WriteLine")))
+          ("c" . (lambda () (interactive) (benj//replace-match-buff "UnityEngine.Debug.Log" "Console\.WriteLine")))
           )))
