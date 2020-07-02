79afa95a443c75b9d80fda78421b270cf1a95875
Benj <Benjamin.Schwerdtner@gmail.com>
2020-07-02 06:08:55 +0200

    Move sharpel keys

--- layers/sharpel/keybindings.el
+++ layers/sharpel/keybindings.el
@@ -1,1 +1,15 @@
-;; TODO move from roslyn layer
+(defconst benj-roslyn-leader-keys "oz")
+
+(spacemacs/declare-prefix benj-roslyn-leader-keys "sharpel")
+
+(progn
+  (mapc (lambda (x)
+          (spacemacs/set-leader-keys (concat benj-roslyn-leader-keys (car x)) (cdr x)))
+        '(("r" . sharpel-logsyntax-req)
+          ("s" . sharpel-refresh-proc)
+          ("F" . sharpel-send-file-name-command)
+          ("l" . sharpel-rerun-last)
+          ("f" . sharpel-rerun-last-file-command)
+          ("d" . sharpel-file-command-on-region)
+          ("c" . sharpel-rewrite-file)
+          ("t" . (lambda () (interactive) (sharpel--start "--try-compilation"))))))
