6683b7ecc32a3278eb81ff5d498855d5ac3864a2
Benj <Benjamin.Schwerdtner@gmail.com>
2020-07-15 14:21:27 +0200

    Swap exluce csharp code funcs

--- layers/csharp-config/keybindings.el
+++ layers/csharp-config/keybindings.el
@@ -1,8 +1,8 @@
 (spacemacs/declare-prefix-for-mode 'csharp-mode "o" "own")
 
 (spacemacs/set-leader-keys-for-major-mode 'csharp-mode "or" 'benj-dotnet-run)
-(spacemacs/set-leader-keys-for-major-mode 'csharp-mode "oe" 'benj-chsarp-exclude-region)
-(spacemacs/set-leader-keys-for-major-mode 'csharp-mode "oE" 'benj-csharp-exclude-buffer)
+(spacemacs/set-leader-keys-for-major-mode 'csharp-mode "oE" 'benj-csharp-exclude-region)
+(spacemacs/set-leader-keys-for-major-mode 'csharp-mode "oe" 'benj-csharp-exclude-buffer)
 (spacemacs/set-leader-keys-for-major-mode 'csharp-mode "oo" 'omnisharp-show-overloads-at-point)
 (spacemacs/set-leader-keys-for-major-mode 'csharp-mode "ol" 'omnisharp-show-last-auto-complete-result)
 (spacemacs/set-leader-keys-for-major-mode 'csharp-mode "os" 'benj-csharp/string-interp-sourrund)
