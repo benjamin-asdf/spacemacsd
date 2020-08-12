3b92932a3ceecdbd19e9196469cf1ccaac0e28c8
Benj <Benjamin.Schwerdtner@gmail.com>
2020-08-12 13:02:05 +0200

    Add chsarp minor mode

---
+++ layers/team-csharp/funcs.el
@@ -0,0 +1,54 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(define-minor-mode team/chsarp-superior-mode
+  "This mode adds chsarp syntax to strings when you put them on multiple lines."
+  :group 'electricity
+  (if team/chsarp-superior-mode
+      (progn
+	      (add-hook 'post-self-insert-hook
+                  #'team/csharp-superior-post-self-insert-function
+        )
+        )
+    (remove-hook 'post-self-insert-hook #'team/csharp-superior-post-self-insert-function)
+    ))
+
+(defun team/csharp-superior-post-self-insert-function ()
+  (when (and team/chsarp-superior-mode
+             (looking-back "\\(\\(if\\)\\|\\(foreach\\)\\)(.*)[[:blank:]]+?{")
+             (looking-at "[^;]+?;"))
+
+    (let ((indent (current-indentation)))
+      (kill-line)
+      (team/->new-line)
+      (yank)
+      (team/->new-line)
+      (indent-line-to indent)
+      (insert "}")
+      (forward-line -1)
+      (evil-normal-state 1)
+      (skip-chars-forward "[:blank:]")
+      )
+    )
+  )
+
+
+(defun team/->new-line ()
+  "Open new line and forward to there."
+  (open-line 1)
+  (forward-line 1))
