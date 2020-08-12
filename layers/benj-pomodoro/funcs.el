dabd4e1c2bfe10a87bbb8a0584d6bf960ea7dc97
Benj <Benjamin.Schwerdtner@gmail.com>
2020-08-12 06:19:43 +0200

    Don't force update mode line org pomodoro

--- layers/benj-pomodoro/funcs.el
+++ layers/benj-pomodoro/funcs.el
@@ -2,3 +2,27 @@
   (setq-default org-pomodoro-manual-break t)
   (setq-default org-pomodoro-short-break-length 3)
   (setq-default org-pomodoro-audio-player (or org-pomodoro-audio-player (executable-find "benaplay"))))
+
+
+
+
+(defadvice org-pomodoro-update-mode-line (around my/org-pom-modeline-adv activate)
+  "See `org-pomodoro-update-mode-line'
+Default update mode line but don't force modeline update"
+  (let ((s (cl-case org-pomodoro-state
+             (:pomodoro
+              (propertize org-pomodoro-format 'face 'org-pomodoro-mode-line))
+             (:overtime
+              (propertize org-pomodoro-overtime-format
+                          'face 'org-pomodoro-mode-line-overtime))
+             (:short-break
+              (propertize org-pomodoro-short-break-format
+                          'face 'org-pomodoro-mode-line-break))
+             (:long-break
+              (propertize org-pomodoro-long-break-format
+                          'face 'org-pomodoro-mode-line-break)))))
+    (setq org-pomodoro-mode-line
+          (when (and (org-pomodoro-active-p) (> (length s) 0))
+            (list "[" (format s (org-pomodoro-format-seconds)) "] "))))
+  ;; (force-mode-line-update t)
+  )
