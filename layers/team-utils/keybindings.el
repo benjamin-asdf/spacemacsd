5b8581a781e54ae9a9a4fcbdfb6f36f6f5b5a265
Benj <Benjamin.Schwerdtner@gmail.com>
2020-08-15 13:04:48 +0200

    Add keyb

--- layers/team-utils/keybindings.el
+++ layers/team-utils/keybindings.el
@@ -1,4 +1,8 @@
 (team/spacemacs-define-keys
  "o0"
  "utils"
- '("r" . team/regex-builder-with-region))
+ '("r" . team/regex-builder-with-region)
+ '("m" . spacemacs/macrostep-transient-state/macrostep-expand))
+
+
+(spacemacs/set-leader-keys "km" 'spacemacs/macrostep-transient-state/macrostep-expand)
