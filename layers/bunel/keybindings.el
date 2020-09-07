17e7f6302a2db278259ced76b3b628d59d40d188
Benj <Benjamin.Schwerdtner@gmail.com>
2020-09-07 11:56:49 +0200

    Cleanup kbds

--- layers/bunel/keybindings.el
+++ layers/bunel/keybindings.el
@@ -1,42 +1,23 @@
 
 
-
-(defconst bunel-leader-keys "ou")
-
-(spacemacs/declare-prefix bunel-leader-keys "unity")
-
-(progn
-  (mapc (lambda (x)
-          (spacemacs/set-leader-keys (concat bunel-leader-keys (car x)) (cdr x)))
-        '(
-
-          )))
-
-(team/spacemacs-define-keys
- "ou"
- "unity"
- '("s" . bunel-save-and-refresh)
- '("S" . (lambda () (interactive) (bunel-save-and-refresh "with-playmode")))
- '("c" . bunel/refresh-and-play)
- '("C" . (lambda () (interactive) (bunel-refresh-client "with-playmode")))
- '("a" . bunel-refresh-all)
- '("A" . (lambda () (interactive) (bunel-refresh-all "with-playmode")))
- '("L" . bunel-open-unity-editor-log)
- '("e" . bunel-execute-debug-method)
- '("o" . bunel-open-overlay)
- '("m" . bunel-open-menu)
- '("p" . bunel-open-prefab)
- '("d" . bunel-open-debug-panel)
- '("g" . bunel-set-globals)
- '("u" . benj-unity/quick-file-usages)
- '("U" . benj-unity/file-usages-with-guid-at-point)
- '("l" . bunel/set-default-project)
- '("L" . bunel/loading-scene-and-play)
- '("0" . bunel-open-unity-editor-log)
-
- )
-
-
+(team/spacemacs-declare-keys
+    "ou"
+    "unity"
+  "c" bunel/refresh-and-play
+  "a" bunel-refresh-all
+  "L" bunel-open-unity-editor-log
+  "e" bunel-execute-debug-method
+  "o" bunel-open-overlay
+  "m" bunel-open-menu
+  "p" bunel-open-prefab
+  "d" bunel-open-debug-panel
+  "g" bunel-set-globals
+  "u" benj-unity/quick-file-usages
+  "U" benj-unity/file-usages-with-guid-at-point
+  "l" bunel/set-default-project
+  "L" bunel/loading-scene-and-play
+  "0" bunel-open-unity-editor-log
+    )
 
 (spacemacs/declare-prefix
   "out"
