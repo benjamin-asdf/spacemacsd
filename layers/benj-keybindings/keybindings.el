1c634c948e2a86a17e35cee7b0115e05305d9832
Benj <Benjamin.Schwerdtner@gmail.com>
2020-09-07 11:57:42 +0200

    Update kbd

--- layers/benj-keybindings/keybindings.el
+++ layers/benj-keybindings/keybindings.el
@@ -35,10 +35,6 @@
 (spacemacs/declare-prefix "on" "new")
 (spacemacs/set-leader-keys "ons" 'benj-new-shell-script)
 
-(spacemacs/set-leader-keys "orr" 'benj-copy-last-yank-to-register)
-(spacemacs/set-leader-keys "orb" 'team/pull-register-2-to-b)
-(spacemacs/set-leader-keys "ore" 'team/last-eldoc-message-to-reg)
-
 (spacemacs/set-leader-keys "occ" 'quick-calc)
 
 (spacemacs/declare-prefix "of" "file")
