ccd656ef99e1fc4bf0e7c056040e37bf43edbdca
Benj <Benjamin.Schwerdtner@gmail.com>
2020-09-09 07:25:34 +0200

    Fix sharp quotes

--- layers/bunel/keybindings.el
+++ layers/bunel/keybindings.el
@@ -3,20 +3,20 @@
 (team/spacemacs-declare-keys
     "ou"
     "unity"
-  "c" bunel/refresh-and-play
-  "a" bunel-refresh-all
-  "L" bunel-open-unity-editor-log
-  "e" bunel-execute-debug-method
-  "o" bunel-open-overlay
-  "m" bunel-open-menu
-  "p" bunel-open-prefab
-  "d" bunel-open-debug-panel
-  "g" bunel-set-globals
-  "u" benj-unity/quick-file-usages
-  "U" benj-unity/file-usages-with-guid-at-point
-  "l" bunel/set-default-project
-  "L" bunel/loading-scene-and-play
-  "0" bunel-open-unity-editor-log
+  "c" #'bunel/refresh-and-play
+  "a" #'bunel-refresh-all
+  "L" #'bunel-open-unity-editor-log
+  "e" #'bunel-execute-debug-method
+  "o" #'bunel-open-overlay
+  "m" #'bunel-open-menu
+  "p" #'bunel-open-prefab
+  "d" #'bunel-open-debug-panel
+  "g" #'bunel-set-globals
+  "u" #'benj-unity/quick-file-usages
+  "U" #'benj-unity/file-usages-with-guid-at-point
+  "l" #'bunel/set-default-project
+  "L" #'bunel/loading-scene-and-play
+  "0" #'bunel-open-unity-editor-log
     )
 
 (spacemacs/declare-prefix
