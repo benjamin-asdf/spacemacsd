2b65b866476308b153a6ffa23be18660e6576bdc
Benj <Benjamin.Schwerdtner@gmail.com>
2020-09-13 06:32:27 +0200

    Add some avy and mc stuff

--- layers/benj-evil-motions/funcs.el
+++ layers/benj-evil-motions/funcs.el
@@ -1,43 +1,107 @@
-(defun benj-avy/take-word ()
-  "Use `evil-avy-goto-subword-1', then yank the word at point."
-  (interactive)
-  (let ((word))
+
+(defun benj-avy/jump (arg action &optional beg end)
+  (setq avy-all-windows t)
+  (avy-with avy-goto-word-0
+    (avy-jump avy-goto-word-0-regexp
+              :window-flip arg
+              :beg beg
+              :end end
+              :action action)))
+
+(defmacro benj-avy/point-action (&rest body)
+  "Return a lambda form that takes one argument, a point.
+Then goto hell with safe excursion and eval BODY.
+Point arg is anaphorically bound to p."
+  `(// (p)
+       (save-excursion
+         (goto-char p)
+         ,@body)))
+
+(defmacro benj-avy/yank-excursion (arg action-form &rest body)
+  "Eval ACTION-FORM at avy destination. Use `avy-jump'.
+When ACTION-FORM evals to non nil, bind it to it and execute BODY."
+  (declare (debug body))
+  (declare (indent 2))
+  `(let ((it))
+     (save-window-excursion
+       (benj-avy/jump
+        ,arg
+        (benj-avy/point-action
+         (setq it ,action-form))))
+     ,@body))
+
+(defun benj-avy/copy-word (&optional arg)
+  (interactive"P")
+  (benj-avy/yank-excursion
+   arg
+   (progn (kill-new (buffer-substring (point) (progn (forward-word) (point)))) t)
+   (yank)))
+
+(defun benj-avy/take-word (&optional arg)
+  (interactive "P")
+  (benj-avy/yank-excursion
+   arg
+   (progn (kill-word 1)
+          (delete-region (point) (progn (forward-word) (point)))
+          t)
+   (yank)))
+
+(defun benj-avy/move-region (&optional arg)
+  (interactive"P")
+  (let ((sel))
     (save-excursion
-      (evil-avy-goto-subword-1)
-      (setq word (thing-at-point 'evil-WORD)))
-    (insert word)))
+      (benj-avy/jump-timer-action
+       arg
+       (// (p)
+           (print p)
+         (setq sel (cons p sel)))))
+    ;; (cl-labels
+    ;sdfg;     ((put-point (place)
+    ;;                 (save-excursion
+    ;;                   (benj-avy/jump-timer-action
+    ;;                    arg
+    ;;                    (// (p) (print p) (setq place (cons p place)))))))
+    ;;   (put-point sel)
+    ;;   ;; (print sel)
+    ;;   ;; (evil-delete (car sel) (cdr sel))
+    ;;   ;; (evil-paste-after))
+    ;;   )
+    )
+  )
+
+(defadvice spacemacs/avy-open-url (around my/spacemacs-avy-open-url-adv activate)
+  (avy-jump "https?://"
+            :action
+            (benj-avy/point-action
+              (browse-url-at-point))))
 
 
-;; todo window bug
-
-(defun benj-avy/take-word-part ()
-  "See `benj-avy/take-word' but use the word part."
-  (interactive)
-  (let ((word)
-        (window-point (window-point)))
-    (save-excursion
-      (evil-avy-goto-char-timer)
-      (setq
-       word
-       (buffer-substring
-        (point)
-        (save-excursion
-          (evil-forward-WORD-end)))))
-    (insert word)
-    (goto-char window-point)))
+(defun benj-avy/jump-timer-action (window-flip action)
+  (setq avy-action (or action avy-action))
+  (let ((avy-all-windows
+         (if window-flip
+             (not avy-all-windows)
+           avy-all-windows)))
+    (avy-with avy-goto-char-timer
+      (setq avy--old-cands (avy--read-candidates))
+      (avy-process avy--old-cands))))
 
 
 
 
-(defun benj-avy/take-region ()
-  "Use `evil-avy-goto-char-timer' twice to get region to take to current postion."
-  (interactive)
-  (let ((beg)
-        (end))
-    (save-excursion
-      (evil-avy-goto-char-timer)
-      (setq beg (point)))
-    (save-excursion
-      (evil-avy-goto-char-timer)
-      (setq end (+ (point) 1)))
-    (insert (buffer-substring beg end))))
+
+
+;; evil mc
+
+(defun my/insert-evil-mc-nums-simple (&optional arg)
+  "Insert a number at each evil mc cursor, incremented by cursor index.
+Start either at 0 or prefix ARG, if given."
+  (interactive"P")
+  (let ((num (or arg 0)))
+    (evil-mc-execute-for-all-cursors
+     (lambda (cursor)
+       (insert
+        (number-to-string
+         (+ num
+            (let ((it (evil-mc-get-cursor-property cursor :index)))
+              (if (= it 0) (length evil-mc-cursor-list) (- it 1))))))))))
--- layers/benj-evil-motions/keybindings.el
+++ layers/benj-evil-motions/keybindings.el
@@ -1,23 +1,14 @@
+(team/spacemacs-declare-keys
+    "om"
+    "motions"
+      "f" #'benj-avy/take-region
+      "i" #'benj-insert-other-line
+      "w" #'benj-avy/take-word
+      "c" #'benj-avy/copy-word
+      "r" #'avy-move-region
+      "l" #'avy-move-line
+      "R" #'benj-avy/move-region
+      "n" #'my/insert-evil-mc-nums-simple
 
 
-
-(defconst benj-motions-leader-keys "om")
-
-(spacemacs/declare-prefix benj-motions-leader-keys "motions")
-
-;; TOOD later today when I'm not sharp anymore
-
-(progn
-  (mapc (lambda (x)
-          (spacemacs/set-leader-keys (concat benj-motions-leader-keys (car x)) (cdr x)))
-        '(("f" . benj-avy/take-region)
-          ("i" . benj-insert-other-line)
-          ("n" . benj-next-digit)
-          ("w" . benj-avy/take-word)
-          ("e" . benj-avy/take-word-part)
-          ("r" . avy-move-region)
-          ("l" . avy-move-line)
-          ("l" . avy-move-line)
-
-
-          )))
+    )
