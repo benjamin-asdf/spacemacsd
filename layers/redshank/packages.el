cd4af3309598e246402742d39835d55b730680dc
Benj <Benjamin.Schwerdtner@gmail.com>
2020-10-09 15:26:38 +0200

    Add redshank

---
+++ layers/redshank/packages.el
@@ -0,0 +1,48 @@
+;;; packages.el --- redshank layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2020 Sylvain Benner & Contributors
+;;
+;; Author: benj <benj@benj-pc>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Commentary:
+
+;;; Code:
+
+(defconst redshank-packages
+  '((redshank-loader :location local)))
+
+(defconst redshank-major-modes
+  '(lisp-mode
+    lisp-interaction-mode
+    emacs-lisp-mode
+    common-lisp-mode))
+
+(defun redshank/init-redshank-loader ()
+
+  (redshank-setup
+   (--map
+    (symb (mkstr it '-hook))
+    redshank-major-modes))
+  (require 'redshank)
+
+  (dolist (mode redshank-major-modes)
+    (spacemacs/declare-prefix-for-mode
+      mode "r" "refactor")
+    (spacemacs/declare-prefix-for-mode
+      mode "rr" "redshank")
+    (--each
+        redshank-keys
+      (cl-destructuring-bind (key . def) it
+        (spacemacs/set-leader-keys-for-major-mode
+          mode
+          (concat "rr" key)
+          def)))))
+
+;;; packages.el ends here
+
+;; (require 'redshank-loader "~/.spacemacs.d/lisp/redshank/redshank-loader.el")
