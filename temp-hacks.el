17da782456052fcd067257f64102340f6e2e9a85
Benj <Benjamin.Schwerdtner@gmail.com>
2020-10-13 16:03:45 +0200

    Hackerinos

--- temp-hacks.el
+++ temp-hacks.el
@@ -1,43 +1,19 @@
 
 
-(defun help--binding-locus (key position)
-  "Describe in which keymap KEY is defined.
-Return a symbol pointing to that keymap if one exists ; otherwise
-return nil.  The argument POSITION is as documented in the
-function `key-binding'."
-  (let ((map (help--key-binding-keymap key t nil position)))
-    (when map
-      (catch 'found
-        (let ((advertised-syms (nconc
-                                (list 'overriding-terminal-local-map
-                                      'overriding-local-map)
-                                (delq nil
-                                      (mapcar
-                                       (lambda (mode-and-map)
-                                         (let ((mode (car mode-and-map)))
-                                           ;; somehow we have an element witch (quote keybinding) in the list
-                                           ;; symbol value quote is throwing
-                                           (with-demoted-errors
-                                               (when (symbol-value mode)
-                                                 (intern-soft
-                                                  (format "%s-map" mode))))))
-                                       minor-mode-map-alist))
-                                (list 'global-map
-                                      (intern-soft (format "%s-map" major-mode))))))
-          ;; Look into these advertised symbols first.
-          (dolist (sym advertised-syms)
-            (when (and
-                   (boundp sym)
-                   (eq map (symbol-value sym)))
-              (throw 'found sym)))
-          ;; Only look in other symbols otherwise.
-          (mapatoms
-           (lambda (x)
-             (when (and (boundp x)
-                        ;; Avoid let-bound symbols.
-                        (special-variable-p x)
-                        (eq (symbol-value x) map))
-               (throw 'found x))))
-          nil)))))
+;; patch up some malformed minor mode
+
+(setq
+ minor-mode-map-alist
+ (--filter
+  (let ((pass nil))
+    (with-demoted-errors
+        (unwind-protect
+            (progn
+              (setq pass nil)
+              (symbol-value (car it))
+              (setq pass t))
+          pass)))
+  minor-mode-map-alist))
+
 
 (provide 'temp-hacks)
