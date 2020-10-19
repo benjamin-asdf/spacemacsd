5e19d65b27213df18217ddb58da0f27b2e068506
Benj <Benjamin.Schwerdtner@gmail.com>
2020-10-19 07:28:01 +0200

    Add lispyville

---
+++ layers/lispy/packages.el
@@ -0,0 +1,103 @@
+
+(defconst lispy-packages
+  '(lispy
+    (evil-lispy :location (recipe :fetcher github
+                                  :repo "sp3ctum/evil-lispy"
+                                  :branch "master"))
+    (targets
+     :location (recipe
+                :fetcher github
+                :repo "noctuid/targets.el"))
+    lispyville))
+
+(defun lispy/init-lispy ()
+  (use-package lispy
+    :defer t
+    :config (spacemacs|diminish lispy-mode "" "")))
+
+(defun lispy/init-evil-lispy ()
+  (use-package evil-lispy
+    :hook ((lisp-mode . lispy-mode)
+          (emacs-lisp-mode . lispy-mode)
+          (scheme-mode . lispy-mode)
+          (racket-mode . lispy-mode)
+          (hy-mode . lispy-mode)
+          (lfe-mode . lispy-mode)
+          (dune-mode . lispy-mode)
+          (clojure-mode . lispy-mode))
+    :commands (evil-lispy-mode)
+    :config (progn
+              (spacemacs|diminish evil-lispy-mode " Ⓛ" " L")
+              (setq lispy-close-quotes-at-end-p t)
+              (add-hook 'lispy-mode-hook #'turn-off-smartparens-mode)
+
+              (when (configuration-layer/package-usedp 'cider)
+
+                ;; todo better mechanism of loading cider
+                (require 'cider)
+                ;; show eval results in a cider overlay, next to point
+                (add-to-list 'lispy-compat 'cider)
+                (setq lispy-eval-display-style 'overlay)))))
+
+;; todo yanking marked stuff is broken
+
+(defun evil-lispy-layer-configure-colorization ()
+  ;; this will be displayed in the modeline
+  (let ((mode-color "light sea green"))
+
+    (defface spacemacs-lispy-face
+      `((t :inherit 'mode-line
+           :background ,mode-color))
+      "lispy state face."
+      :group 'spacemacs)
+
+    (setq evil-lispy-state-cursor '(mode-color box))
+
+    (setq evil-lispy-state-cursor
+          (list (when dotspacemacs-colorize-cursor-according-to-state mode-color)
+                'box))))
+
+(defun lispy/init-targets ()
+  (use-package targets
+    :defer t))
+
+
+(defun lispy/init-lispyville ()
+  (use-package lispyville
+    :hook (lispy-mode . lispyville-mode)
+    :init
+    (setq lispyville-key-theme
+          '((operators normal)
+            c-w
+            (prettify insert)
+            (atom-movement t)
+            slurp/barf-lispy
+            additional
+            additional-insert
+            text-objects))
+    :config
+    (progn
+      (lispyville-set-key-theme)
+      (require 'targets)
+      (require 'evil)
+      (setq targets-text-objects nil)
+      (targets-setup) ;;; foo
+      (targets-define-to lispyville-comment 'lispyville-comment nil object
+                         :last-key nil
+                         :bind t :keys "c")
+      (targets-define-to lispyville-atom 'lispyville-atom nil object
+                         :last-key nil
+                         :bind t :keys "a")
+      (targets-define-to lispyville-list 'lispyville-list nil object
+                         :last-key nil
+                         :bind t :keys "L")
+      (targets-define-to lispyville-sexp 'lispyville-sexp nil object
+                         :last-key nil
+                         :bind t :keys "x")
+      (targets-define-to lispyville-function 'lispyville-function nil object
+                         :last-key nil
+                         :bind t :keys "f")
+      (targets-define-to lispyville-string 'lispyville-string nil object
+                         :last-key nil
+                         :bind t :keys "S")
+      (spacemacs|diminish lispyville-mode " Ⓥ" " V"))))
