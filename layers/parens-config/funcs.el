19017b2cfec9e5eb93656152f35dd8e70163e4a2
Benj <Benjamin.Schwerdtner@gmail.com>
2020-10-28 11:13:01 +0100

    Add my toggle show paren

--- layers/parens-config/funcs.el
+++ layers/parens-config/funcs.el
@@ -1,3 +1,18 @@
+(defun my/toggle-parens-display (&optional explicit)
+  (interactive)
+  (setq
+   show-paren-style
+   (or
+    explicit
+    (cadr (memql show-paren-style '(parenthesis expression mixed)))
+    'parenthesis))
+  (if (eq show-paren-style 'parenthesis)
+      (set-face-underline 'show-paren-match "White")
+    (set-face-underline 'show-paren-match nil))
+  (message "%s is now %s"
+           (mkstr 'show-paren-style)
+           (mkstr show-paren-style)))
+
 ;; FIXME make it work, crashes atm around line delimeters
 (with-eval-after-load 'smartparens
   (show-smartparens-global-mode -1)
@@ -5,6 +20,7 @@
 
   ;; sticking to show paren mode instead
   (set-face-foreground 'show-paren-match "White")
-  (set-face-underline 'show-paren-match "White")
+  (my/toggle-parens-display 'parenthesis)
+
 
   (show-paren-mode 1))
