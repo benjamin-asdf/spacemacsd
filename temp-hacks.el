c914009acec1d066a447e6958e0be7e079a0bc0c
Benj <Benjamin.Schwerdtner@gmail.com>
2021-01-22 18:12:02 +0100

    Add func for fixing omnisharp requires

--- temp-hacks.el
+++ temp-hacks.el
@@ -42,4 +42,52 @@
         t)
      (replace-match "//"))))
 
+
+(defun my/temp-fix-omnisharp-requires ()
+  (--map
+   (team/with-file
+    it
+    (unless
+        (re-search-forward "require 'cl" nil t)
+      (forward-line 15)
+
+      (insert
+       "
+(require 'cl)
+(require 'cl-lib)
+(require 'csharp-mode)
+(require 'json)
+(require 'files)
+(require 'ido)
+(require 'thingatpt)
+(require 'dash)
+(require 'compile)
+(require 'dired)
+(require 'popup)
+(require 'etags)
+(require 'flycheck)
+(require 's)
+(require 'f)
+"))
+
+    )
+   (directory-files
+    "/home/benj/.emacs.d/elpa/28.0/develop/omnisharp-20201220.906/"
+    t
+    ".el$"))
+
+  (-map
+   #'delete-file
+   (directory-files
+    "/home/benj/.emacs.d/elpa/28.0/develop/omnisharp-20201220.906/"
+    t
+    ".elc$")))
+
+
+
+
+
+
+
+
 (provide 'temp-hacks)
