928c7e4f89d7ed95e2a2990e9e1743510396b0e9
Benj <Benjamin.Schwerdtner@gmail.com>
2021-02-15 18:13:49 +0100

    Add some early init

---
+++ early-init.el
@@ -0,0 +1,12 @@
+(defvar team-elisp-dir "~/repos/lisp/team-elisp/")
+
+(dolist
+    (file
+     (directory-files-recursively
+      (concat team-elisp-dir "lisp/")
+      ".el"))
+  (cl-pushnew
+   (expand-file-name
+    (file-name-directory file))
+   load-path
+   :test 'equal))
