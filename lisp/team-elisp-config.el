e67a93383521b82623053bdfe60da60b750c97d3
Benj <Benjamin.Schwerdtner@gmail.com>
2021-02-15 18:32:15 +0100

    Migrate stuff to team repo

--- lisp/team-elisp-config.el
+++ lisp/team-elisp-config.el
@@ -1,31 +1,12 @@
 (require 'use-package)
 
-(defvar team-elisp-dir "~/repos/lisp/team-elisp/")
-
-(dolist
-    (file
-     (directory-files-recursively
-      (concat team-elisp-dir "lisp/")
-      ".el"))
-  (cl-pushnew
-   (expand-file-name
-    (file-name-directory file))
-   load-path
-   :test 'equal))
-
-(use-package
-  our-editor-tools
-  :init
-  (team/spacemacs-declare-keys
-      "oe"
-      "external"
-    "1"
-    #'benj/last-editor-msg))
-
-
 (use-package game-log-tools)
 
 (use-package helm-csharp-enums :defer t)
+
+(use-package
+  helm-stuff
+  :after 'helm)
 
 (use-package
   benj-helm-secretary
